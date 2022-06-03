5d6ceca00d0cc56ee922733ce065680c769cc18a
pikunena <sherar007@gmail.com>
2022-06-02 21:20:39 -0300

    tarea finalizada

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,23 +1,31 @@
 // Do not change any of the function names
 
 function mayuscula(nombre) {
+  return nombre[0].toUpperCase() + nombre.slice(1)
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
 }
 
 function invocarCallback(cb) {
+  cb();
   // Invoca al callback `cb`
   //Tu código:
 }
 
 function operacionMatematica(n1, n2, cb) {
+ return cb(n1, n2);
+  
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
 }
 
 function sumarArray(numeros, cb) {
+  let sumaReduce = numeros.reduce(function(acu,ele){
+    return acu + ele;
+  });
+  cb(sumaReduce);
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
@@ -25,12 +33,19 @@ function sumarArray(numeros, cb) {
 }
 
 function forEach(array, cb) {
+  array.forEach(function(ele){
+    cb(ele);
+  });
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
 }
 
 function map(array, cb) {
+  nuevoarray = array.map(function(ele){
+      return cb(ele);
+  }); 
+  return nuevoarray;
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
@@ -38,6 +53,9 @@ function map(array, cb) {
 }
 
 function filter(array) {
+   return array.filter(function(ele){
+    return ele[0]=== 'a';
+  })
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
